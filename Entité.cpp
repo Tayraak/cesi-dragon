7519e62259cf7cb9f3d33ddbaac5397008daed00
Tayraak <gabydelamotte@gmail.com>
2023-11-20 10:21:54 +0100

    AjoutEntité

---
+++ Entité.cpp
@@ -0,0 +1,1 @@
+#include "Entité.h"
---
+++ Entité.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+class Entite
+{
+public:
+	std::string nom;
+	int pv;
+	int attaque;
+
+};
